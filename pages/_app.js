9ebf3c29160131269a46d9ffa5569943b478b272
Ahmed <sobh7771@gmail.com>
2022-01-26 06:47:00 +0200

    Remove  class after page loaded

--- pages/_app.js
+++ pages/_app.js
@@ -13,7 +13,7 @@ function MyApp({ Component, pageProps }) {
 
 	return (
 		<QueryClientProvider client={client}>
-			<div className={isBrowser && "preload"}>
+			<div className={!isBrowser && "preload"}>
 				<Component {...pageProps} />
 			</div>
 			<GlobalStyle />
